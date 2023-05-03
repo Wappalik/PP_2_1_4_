f510656c24dd745feacda64158d64a8c0cb3a7ad
Wappalik <baxriddinov_97@mail.ru>
2023-05-03 14:50:14 +0300

    updated

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,17 @@
 package app.model;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component ("timer")
+@Scope ("singleton")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
+
 
     public Long getTime() {
         return nanoTime;
